35d2621b883c6793a5721271621f4f5d89afdfb1
IreneMonlleo <irene.monlleo@gmail.com>
2022-03-25 19:30:42 +0100

    structure slider about us

--- js/slider.js
+++ js/slider.js
@@ -1,3 +1,5 @@
+
+//Home//
 const swiper = new Swiper('.swiper', {
   // Optional parameters
   loop: true,
@@ -9,5 +11,25 @@ const swiper = new Swiper('.swiper', {
 });
 
 
+//ABOUT US//
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
 
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
 
