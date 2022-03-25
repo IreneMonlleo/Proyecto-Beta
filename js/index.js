8d0afed39e14b3d38fc933436ad89cbf9e4d8747
IreneMonlleo <irene.monlleo@gmail.com>
2022-03-25 18:29:41 +0100

    js

--- js/index.js
+++ js/index.js
@@ -1,21 +1,66 @@
+//llama al json//
+function getJSON() {
 
-fetch('/resources/products.json')
-  .then(function(resources) { return resources.json() })
-  .then(function(products) {
+    fetch('/resources/products.json')
+        .then(function (resources) {
+            return resources.json();
+        }).then(function (products) {
+            //por defecto al iniciar muestra todo//
+            identifyCategory("all", products);
+            initialFunction(products);
+        })
+}
 
-    console.log(products);   
-   })
+//recoje los elementos del Json segun la categoria//
+function identifyCategory(category, products) {
 
-   function crearMenu() {
-       
-   }
-   products.forEach(function(product){
-       document.querySelector('.father-grid').innerHTML += `<img>${decor.image}`
-   })
+    let elements = [];
+    if (category == "all") {
+        products.furniture.elements.forEach(function (product) {
+            elements.push(product);
+        });
+        products.illumination.elements.forEach(function (product) {
+            elements.push(product);
+        });
+        products.decor.elements.forEach(function (product) {
+            elements.push(product);
+        });
+    } else {
+        elements.push(products[category].elements);
+    }
 
-   document.querySelectorAll('.filter').forEach(function(button) {
-       button.addEventListener('click', function() {
-           alert("hola");
+    paintElements(elements);
+}
 
-       })
-   })
+function paintElements(elements) {
+    let father = document.querySelector('#father--grid');
+    elements.forEach(function (element) {
+        ´${name.image}
+    })
+}
+
+//dar funcionalidad a los botones//
+function initialFunction(products) {
+
+    document.querySelectorAll('.filter').forEach(function (button) {
+        button.addEventListener('click', (ev) => {
+            showCategorie(products, ev.target);
+        })
+    })
+}
+
+//identifica la classe del boton//
+function showCategorie(products, button) {
+
+    let category = button.classList[1];
+    identifyCategory(category, products);
+}
+
+// iniciar el navegador//
+function init() {
+
+    //función de la promesa y sacar el Json//
+    getJSON();
+}
+
+window.onload = init();
