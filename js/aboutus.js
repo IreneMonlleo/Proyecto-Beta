592607adaac550aa96034832d787bf9b6bdf6c90
IreneMonlleo <irene.monlleo@gmail.com>
2022-03-29 23:47:09 +0200

    about slider links

--- js/aboutus.js
+++ js/aboutus.js
@@ -3,9 +3,9 @@ function paintElements(elements) {
   elements.forEach(function (element) {
       console.log(element.image);
       container.innerHTML = container.innerHTML + `
-      <div class="swiper-slide">
-          <img src="${element.image}" alt="${element.alt}">
-      </div>`
+      <a class="swiper-slide" href="${element.href}">
+          <img class="about__swiper__img" src="${element.image}" alt="${element.alt}">
+      </a>`
   })
 }
 
@@ -17,19 +17,33 @@ getJSON(function(products) {
 
 const initSwiper = function() {
   //ABOUT US//
-  var swiperAbout = new Swiper(".mySwiper", {
+  var swiper = new Swiper(".swiper", {
     slidesPerView: 3,
-    spaceBetween: 30,
+    spaceBetween: 15,
     slidesPerGroup: 3,
     loop: true,
     loopFillGroupWithBlank: true,
     pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+        el: ".swiper-pagination",
+        clickable: true,
     },
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
-  });
+    breakpoints: {
+    280: {
+        slidesPerView: 1,
+        spaceBetween: 15,
+    },
+    500: {
+        slidesPerView: 2,
+        spaceBetween: 15,
+    },
+    1081: {
+        slidesPerView: 3,
+        spaceBetween: 15,
+    },
+    },
+  })
 }
--- js/slider.js
+++ js/slider.js
@@ -12,18 +12,4 @@ const swiper = new Swiper('.swiper-one-image', {
 
 
 //ABOUT US//
-var swiperAbout = new Swiper(".swiper-three-images", {
-  slidesPerView: 3,
-  spaceBetween: 30,
-  slidesPerGroup: 3,
-  loop: true,
-  loopFillGroupWithBlank: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
+
