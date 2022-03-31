b8a60e80c251333c33915b06f0fcb15befc984c7
IreneMonlleo <irene.monlleo@gmail.com>
2022-04-01 01:02:15 +0200

    style slider about

--- js/aboutus.js
+++ js/aboutus.js
@@ -3,9 +3,9 @@ function paintElements(elements) {
   elements.forEach(function (element) {
       console.log(element.image);
       container.innerHTML = container.innerHTML + `
-      <a class="swiper-slide slider__about" href="${element.href}">
+        <a class="swiper-slide slider__about" style="width: 200px; margin-right: 12px;" href="${element.href}">
           <img class="about__slider__img" src="${element.image}" alt="${element.alt}">
-      </a>`
+        </a>`
   })
 }
 
