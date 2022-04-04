a50de93423054c0836a250ee77a551681ef2e6fb
IreneMonlleo <irene.monlleo@gmail.com>
2022-04-04 02:29:47 +0200

    modal checkout style

---
+++ js/checkout.js
@@ -0,0 +1,13 @@
+const params = new URLSearchParams(window.location.search);
+        const section = params.get('section');
+        const productId = params.get('product');
+
+        getJSON(function(products) {
+          const category = products[params.get('section')];
+          const product = category.elements.find(function(product) { return product.id === productId; });
+          console.log(product);
+          document.querySelector('.checkout--name').textContent = product.name;
+          document.querySelector('.checkout--image').src = product.image;
+          document.querySelector('.products--price').textContent = product.price;
+        })
+    
